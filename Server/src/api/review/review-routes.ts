ec340103e665e75ac21d08d5ccae1dc8aee214d3
rbumatay <rbumatay@csu.fullerton.edu>
2025-04-18 13:36:48 -0700

    comments

--- Server/src/api/review/review-routes.ts
+++ Server/src/api/review/review-routes.ts
@@ -4,6 +4,7 @@ import { Auth } from '../../utilities/auth';
 
 export class ReviewRoutes {
   static init(router: express.Router): void {
+    // User must be logged in to post and delete reviews
     router.route('/api/review/:id')
       .get(ReviewController.getReviews)
       .post(Auth.protected, ReviewController.addReview)
