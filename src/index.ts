75339e19e7baea32ef95393bfd832991991f9e20
Luuser <jason.luuop@gmail.com>
2025-04-27 00:42:30 -0700

    Fix: Update CORS allowedHeaders

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,9 @@ app.use(cors({
 	origin: ['https://elaborate-yeot-7e93c3.netlify.app', 'http://localhost:5173'],
 	credentials: true,
 	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
-}));
+	allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+  }));
+  
 
 // Parse JSON bodies
 app.use(express.json());
